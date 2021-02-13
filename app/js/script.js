d74e943c3f3b71902beab24eb9fd6a76b7c83c95
Graeme <graemster@gmail.com>
2021-02-12 23:27:39 -0600

    push header elements to front of z-index stack to make visible over gradient

--- app/js/script.js
+++ app/js/script.js
@@ -1,44 +1,31 @@
-
 const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 
+function closeMobileMenu() {
+	console.log('hamburger clicked.');
+	console.log('triggered close menu event');
+	// Close Hamburger Menu
+	body.classList.remove('noscroll');
+
+	header.classList.remove('open');
+	fadeElems.forEach(function (element) {
+		element.classList.remove('fade-in');
+		element.classList.add('fade-out');
+	});
+	overlay.classList.remove('fade-in');
+	overlay.classList.add('fade-out');
+}
+
 btnHamburger.addEventListener('click', function () {
-	console.log('open hamburger');
+	console.log('hamburger clicked.');
 	if (header.classList.contains('open')) {
-		// Close Hamburger Menu
-		body.classList.remove("noscroll")
-
-		header.classList.remove('open');
-		fadeElems.forEach(function (element) {
-			element.classList.remove('fade-in');
-			element.classList.add('fade-out');
-		});
-		overlay.classList.remove('fade-in');
-		overlay.classList.add('fade-out');
-    
-		// click overlay to close menu too
-		overlay.addEventListener('click', function () {
-			if (header.classList.contains('open')) {
-				// Close Hamburger Menu
-				body.classList.remove('noscroll');
-		
-				header.classList.remove('open');
-				fadeElems.forEach(function (element) {
-					element.classList.remove('fade-in');
-					element.classList.add('fade-out');
-				});
-				overlay.classList.remove('fade-in');
-				overlay.classList.add('fade-out');
-			}
-			overlay.removeEventListener('click');
-		});
-
+		closeMobileMenu();
 	} else {
 		//Open Hamburger Menu
-		body.classList.add("noscroll")
+		body.classList.add('noscroll');
 		header.classList.add('open');
 		fadeElems.forEach(function (element) {
 			element.classList.remove('fade-out');
@@ -46,5 +33,12 @@ btnHamburger.addEventListener('click', function () {
 		});
 		overlay.classList.remove('fade-out');
 		overlay.classList.add('fade-in');
+	}
+});
+
+// click overlay to close menu too
+overlay.addEventListener('click', function () {
+	if (header.classList.contains('open')) {
+		closeMobileMenu();
 	}
 });
